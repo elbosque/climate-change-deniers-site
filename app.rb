6485c512352fa53e816823e1f6818edbf9863852
Corey Speisman <Cspeisman@gmail.com>
2014-11-16 14:27:45 -0500

    used octokit for github api

--- app.rb
+++ app.rb
@@ -1,27 +1,13 @@
 require 'sinatra'
 require 'net/http'
 require 'uri'
-
+require 'octokit'
 get '/' do 
   erb :index
 end
 
-get '/callback' do
-  
-  res = Net::HTTP.post_form URI('https://github.com/login/oauth/access_token'), { "client_id" => "aca900b2e5f04bd96f00", "client_secret" => "115bd0f23baa54b3ff57906d0184a74889931125", "code" => params['code']}
 
-  access_token = /access_token=\S*[&]/.match(res.body)[0].split('=')[1]
-  p access_token
-  Net::HTTP.get URI("https://api.github.com/v3/orgs/elbosque/issues")
-end
-
-get '/response' do
-end
-
-get "/test" do
-  # test = Net::HTTP.post_form URI('https://api.github.com/repos/elbosque/climate-change-positions/issues'),
-                    # { "title" => "TEST TEST", "body" => "Ham Sandwich" }
-  test = Net::HTTP.get URI('https://api.github.com/user/repos')
-  p test
-  redirect to('/')
+get "/create-issue" do
+  client = Octokit::Client.new(:login => 'clime-hater', :password =>'waffles1234') 
+  client.create_issue('elbosque/climate-change-positions', 'Climehate', 'Testhing this thing out', {:labels => 'label' })
 end

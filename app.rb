620bf2bd1446db23f106530e301f3555ede59d60
Corey Speisman <Cspeisman@gmail.com>
2014-11-16 15:23:03 -0500

    form and submit

--- app.rb
+++ app.rb
@@ -3,16 +3,22 @@ require "sinatra/config_file"
 require 'net/http'
 require 'uri'
 require 'octokit'
+require 'yaml'
 
-config_file 'config.yml'
+config_file './config.yml'
 
 
 get '/' do 
   erb :index
 end
 
+post '/parse-form' do
+  create_issue(params)
+end
 
-get "/create-issue" do
+private
+
+def create_issue(options = {})
   client = Octokit::Client.new(:login => 'clime-hater', :password =>'waffles1234') 
-  client.create_issue('elbosque/climate-change-positions', 'Climehate', 'Testhing this thing out', {:labels => 'label' })
+  client.create_issue('elbosque/climate-change-positions', 'Climehate', options.to_yaml, {:labels => 'label' })
 end

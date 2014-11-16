1f42ddd5f923dee3766d121e46719666bfbdcaa3
polar3x <elpolar3x@gmail.com>
2014-11-16 15:16:15 -0500

    Update Gemfile and app for mongo

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
-source 'http://rubygems.org'
+source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'sinatra-contrib'
 gem 'rake'
+gem 'mongo'
+gem 'bson_ext'
+gem 'byebug'
--- app.rb
+++ app.rb
@@ -3,9 +3,18 @@ require "sinatra/config_file"
 require 'net/http'
 require 'uri'
 require 'octokit'
+require 'json/ext'
+require 'mongo'
 
 config_file 'config.yml'
 
+include Mongo
+
+configure do
+  conn = MongoClient.new("localhost", 27017)
+  set :mongo_connection, conn
+  set :mongo_db, conn.db('test')
+end
 
 get '/' do 
   erb :index

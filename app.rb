a4414c5200db130aa2cdaeeb71e6d919445d47eb
polar3x <elpolar3x@gmail.com>
2014-11-17 23:52:37 -0500

    wrongheadedly grab file we already have from GitHub API

--- app.rb
+++ app.rb
@@ -47,7 +47,7 @@ end
 #end
 
 post '/parse-form' do
-  create_issue(params)
+  create_pull_request(params)
 end
 
 private
@@ -56,7 +56,34 @@ def get_file(bioguide)
 
 end
 
-def create_issue(options = {})
+def create_pull_request(options = {})
   client = Octokit::Client.new(:login => ENV['GITHUB_NAME'], :password => ENV['GITHUB_PASS']) 
-  client.create_issue('elbosque/climate-change-positions', options["bioguide"], options.to_yaml, {:labels => 'label' })
+  #client.create_issue('elbosque/climate-change-positions', options["bioguide"], options.to_yaml, {:labels => 'label' })
+  yaml_blob_hash = `git hash-object #{ENV['CCD_DATA_DIRECTORY']}/climate-change-positions/politicians/#{options["bioguide"]}.yml`
+  yaml_blob = client.blob("clime-hater/scratch", yaml_blob_hash.strip())
+  current_leg_hash = YAML.load(Base64.decode64(yaml_blob[:content]))
+  return false unless has_all_options?(options)
+  new_leg_hash = resolve_yaml_disputes(current_leg_hash, options)
+end
+
+def has_all_options?(options)
+  options.keys.sort == ["proof", "statement_date", "bioguide", "exist", "anthropogenic", "evasive"].sort
+end
+
+def resolve_yaml_disputes(current_leg_hash, options)
+  if current_leg_hash["statements"].map{|s|s["proof"]}.include?(options["statement"])
+    #statement's already in there. deal with this.
+  else
+    current_leg_hash["statements"].push({ 
+      "climate_change_stances" => {
+        "anthropogenic"=> options["anthropogenic"],
+        "exists"=> options["exist"]
+      }, 
+      "date" => options["statement_date"],
+      "excerpt" => options["excerpt"],
+      "proof" => options["proof"],
+      "evasive" => options["evasive"]
+    })
+  end
+  current_leg_hash
 end
